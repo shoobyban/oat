3196a276de980e9c51cb0eff49f760a22735fe8e
Kailash Nadh <kailash@nadh.in>
2026-02-20 14:30:39 +0530

    Dismiss sidebar when clicked outside on mobile view. Closes #91.

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -7,5 +7,16 @@ document.addEventListener('click', (e) => {
   if (toggle) {
     const layout = toggle.closest('[data-sidebar-layout]');
     layout?.toggleAttribute('data-sidebar-open');
+    return;
+  }
+
+  // Dismiss sidebar when clicking outside (when sidebar is not an overlay).
+  if (!e.target.closest('[data-sidebar]')) {
+    const layout = document.querySelector('[data-sidebar-layout][data-sidebar-open]');
+    // Hardcode breakpoint (for now) as there's no way to use a CSS variable in
+    // the @media{} query which could've been picked up here.
+    if (layout && window.matchMedia('(max-width: 768px)').matches) {
+      layout.removeAttribute('data-sidebar-open');
+    }
   }
 });
